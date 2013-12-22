b60aebd309dd90f9ccd3f4702b0c8df7745b75f8
gjhenrique <gjhenrique@gmail.com>
2013-12-22 18:59:48 -0200

    Testes do controller para testar login no sistema

---
+++ spec/controllers/login_controller_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+
+describe ItemsController do
+
+	login_user
+
+	it "user exists in session"
+		expect(subject.current_user).not_to be_nil
+	end
+
+	it "enter initial page with success" do
+		get :index
+		expect(response.status).to eq(200), "Does not load initial page"
+	end	
+
+end
