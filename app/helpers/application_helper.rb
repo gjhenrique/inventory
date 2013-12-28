6af3bdc38f70f784c880f5e1cafbcfc585d6a5f9
gjhenrique <gjhenrique@gmail.com>
2013-12-28 12:19:28 -0200

    Estrutura inicial para cadastro de novos usuarios pelo administrador

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,4 +5,10 @@ module ApplicationHelper
 			t title
 		end
 	end
+
+	def print_notification
+		content_tag(:div, flash[:error], class: "alert alert-danger", :id => "flash_error") if flash[:error]
+	 	content_tag(:div, flash[:notice], class: "alert alert-success", :id => "flash_success") if flash[:notice] 
+	  content_tag(:div, flash[:alert], class: "alert alert-warning", :id =>"flash_alert") if flash[:alert] 
+	end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :trackable
+  devise :database_authenticatable, :trackable, :registerable, :validatable
+
+  def admin?
+  	admin
+  end
 end
