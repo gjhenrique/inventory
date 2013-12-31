d3b07fd3fbdc988fe144a3bd2abbfdc912b7ca49
gjhenrique <gjhenrique@gmail.com>
2013-12-31 12:05:39 -0200

    Formulario de edicao e criacao de usuarios prontos

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,7 +2,7 @@ class RegistrationsController < Devise::RegistrationsController
 
 	skip_before_filter :require_no_authentication
 
-	load_and_authorize_resource class: User
+	load_and_authorize_resource class: User, except: [:edit, :update]
 
 	def create
 		super do |resource|
