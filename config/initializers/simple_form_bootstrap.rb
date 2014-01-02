a4015d8325fa303c6b1d97f2dd15c8bb36ccb662
gjhenrique <gjhenrique@gmail.com>
2014-01-02 18:32:23 -0200

    Padronizando estilo dos formularios

--- config/initializers/simple_form_bootstrap.rb
+++ config/initializers/simple_form_bootstrap.rb
@@ -1,11 +1,12 @@
 # Use this setup block to configure all options available in SimpleForm.
 SimpleForm.setup do |config|
-  config.wrappers :bootstrap, tag: 'div', class: 'form-group', error_class: 'has-error' do |b|
+  config.wrappers :bootstrap, tag: 'div', label_html: {class: 'col-xs-3' }, html: {class: 'form-horizontal'},
+  class: 'form-group', error_class: 'has-error' do |b|
     b.use :html5
     b.use :placeholder
     b.use :label
 
-    b.wrapper :right_col, tag: :div do |component|
+    b.wrapper :right_col, tag: :div, class: 'col-xs-6' do |component|
       component.use :input
       component.use :hint,  wrap_with: { tag: 'p', class: 'help-block' }
       component.use :error, wrap_with: { tag: 'span', class: 'help-block has-error' }
@@ -43,4 +44,6 @@ SimpleForm.setup do |config|
   # to learn about the different styles for forms and inputs,
   # buttons and other elements.
   config.default_wrapper = :bootstrap
+
+
 end
