72fb78527a751e32e6d74425b5f8409699280955
Guilherme Henrique <gjhenrique@gmail.com>
2014-02-03 16:33:54 -0200

    Adding nested attributes of line item in order with model test

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,4 +4,6 @@ class Order < ActiveRecord::Base
 
   has_many :line_items
   has_many :items, through: :line_items
+
+  accepts_nested_attributes_for :line_items, reject_if: :all_blank
 end
--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -4,4 +4,7 @@ describe Order do
 
   it { should have_many(:line_items)}
   it { should have_many(:items)}
+
+  it { should accept_nested_attributes_for(:line_items) }
+
 end
